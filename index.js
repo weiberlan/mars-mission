fd9ec18d7d49c089f3401df555ab9f59681c5231
weiberlan <weiberlan@gmail.com>
2022-07-15 04:39:51 -0300

    removing TODOS from code

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import Mission from './app/Mission.js';
-// TODO: import and use a validator lib
-// TODO: refactor processMissionData method
 
 const mission = new Mission('./app/input/data.txt');
 mission.runMission().then((data) => console.log(data));
